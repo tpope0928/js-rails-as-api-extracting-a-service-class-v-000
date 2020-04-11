91de960e27d55727e80608fc9342c1c61d19e12b
Thomas Pope <tpope0928@gmail.com>
2020-04-11 20:52:02 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -5,10 +5,18 @@ class SightingSerializer
   end
 
   def to_serialized_json
-    @sighting.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
-  end
+  options = {
+    include: {
+      bird: {
+        only: [:name, :species]
+      },
+      location: {
+        only: [:latitude, :longitude]
+      }
+    },
+    except: [:updated_at],
+  }
+  @sighting.to_json(options)
+end
 
 end
